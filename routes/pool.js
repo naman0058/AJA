1f2d00367bdb0432648d1543a3fadbb97df5b3dd
naman0058 <jnaman345@gmail.com>
2021-10-04 12:22:54 +0530

    a

--- routes/pool.js
+++ routes/pool.js
@@ -7,7 +7,7 @@ const pool = mysql.createPool({
  host : 'db-mysql-blr1-40413-do-user-9900615-0.b.db.ondigitalocean.com',
    user: 'doadmin',
     password : 'dcrrYttrd1zr4duH',
-    database: 'AJA',
+    database: 'aja',
     port:'25060' ,
     multipleStatements: true
   })
